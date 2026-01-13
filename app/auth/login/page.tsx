3c4bd4fdf7db39c9c17775554e7fbbb02869de61
JerickTwO <jerickjjtm774@gmail.com>
2026-01-13 09:57:26 -0500

    initial project

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import { BookmarkIcon } from "@radix-ui/react-icons"
+import { Button } from "@radix-ui/themes"
+
+function LoginPage() {
+  return (
+    <>
+    <div>LoginPage</div>
+    <Button>
+        <BookmarkIcon></BookmarkIcon>
+        BookMark</Button>
+    </>
+  )
+}
+
+export default LoginPage
